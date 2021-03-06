8bdf98b6ec4ad5b5125437953e5f86a3cf06e8a4
CTRL-soap <80178221+CTRL-soap@users.noreply.github.com>
2021-03-06 23:32:54 +0100

    Add files via upload

---
+++ pass-gen.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWYXZ";
+
+    char *Password;
+
+    int lenght;
+    int var;
+
+    abc:
+    printf("ENTER THE LENGHT OF THE PASSWORD: ");
+    scanf("%d", &lenght);
+    printf("SPECIAL CHARACTERS? (0/1): ");
+    scanf("%d", &var);
+
+    if(var != 1 && var != 0 ){
+    	printf("ERROR\n");
+    	goto abc;
+	}
+
+    Password = (char *)calloc(lenght, sizeof(char));
+    
+    srand((unsigned)time(NULL));
+    
+    if(var == 1){
+       char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWYXZ-_.!#$&?*%~@+";
+    	
+       for(int i = 0; i < lenght; i++) {
+       Password[i] = charset[rand() % 75];
+    }
+	}
+	
+    else{
+       for(int i = 0; i < lenght; i++) {
+       Password[i] = charset[rand() % 62];
+    }
+    }
+
+    printf("\nPASSWORD: \n");
+    printf("\n %s \n", Password);
+    
+    return 0;
+    }
